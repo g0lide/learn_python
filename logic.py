a4155400eebc77ff9a06d113d2a2f631d36a6392
Golide <mbongenitshuma@gmail.com>
2022-02-21 21:50:07 +0200

    Use elif and else to create simple logic in programs.

---
+++ logic.py
@@ -0,0 +1,18 @@
+print("What is your favourite football team?")
+team = input()
+if team == "Chelsea":
+    print("You've got excellent taste.")
+elif team == "Arsenal":
+    print("You must be very patient.")
+elif team == "Man United":
+    print("You must miss Alex Ferguson.")
+elif team == "Man City":
+    print("Also a Barcelona fan I presume?")
+elif team == "Liverpool":
+    print("Rock on!.")
+elif team == "Real Madrid":
+    print("You were probably rooting for Thanos in Avengers, collect them all!")
+elif team == "Ajax":
+    print("Respect!")
+else:
+    print("Interesting!")
